517a68a5df15a44a83bf90861b2379ab6ffef429
MuhRezkyPasuloi <resky.pasuloi@gmail.com>
2024-03-27 12:34:07 +0800

    commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(window).on("resize", function() {
+    var width = $(window).width();
+    if (width > 768) {
+      $(".navbar-brand").css("height", "50px");
+      $(".navbar-brand").css("width", "auto");
+    } else {
+      $(".navbar-brand").css("height", "30px");
+      $(".navbar-brand").css("width", "auto");
+    }
+  });
+
+  function filterSelection(category) {
+    var cards = document.querySelectorAll('.card');
+    if (category === 'all') {
+        cards.forEach(function(card) {
+            card.style.display = 'block';
+        });
+    } else {
+        cards.forEach(function(card) {
+            if (card.classList.contains(category)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
+}
